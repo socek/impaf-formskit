72e934f3fd6b57e91d873ff983c037f1ebcc2ae1
Dominik "Socek" Dlugajczyk <msocek@gmail.com>
2015-07-25 21:38:37 +0200

    Add FormFixture.

--- src/implugin/formskit/testing.py
+++ src/implugin/formskit/testing.py
@@ -1,9 +1,11 @@
 from mock import patch
 
-from pytest import yield_fixture
 from pytest import fixture
+from pytest import yield_fixture
+from pytest import mark
 
 from impaf.testing import ControllerFixture
+from impaf.testing import RequestFixture
 
 
 class FormskitControllerFixture(ControllerFixture):
@@ -17,3 +19,10 @@ class FormskitControllerFixture(ControllerFixture):
     @fixture
     def fform(self, madd_form):
         return madd_form.return_value
+
+
+class FormFixture(RequestFixture):
+
+    @fixture
+    def testable(self, mrequest, registry):
+        return self._testable_cls(mrequest)
